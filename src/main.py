3cf0fe5b076bbb24f2ddd1e9fffa2ef35ad82244
Jbud <Jbudn90@yahoo.com>
2020-05-15 01:44:40 -0600

    Code is finished. Images generated. Need to complete Readme presentation

--- src/main.py
+++ src/main.py
@@ -31,9 +31,6 @@ plt.close('all')
 
 threshold = 100
 
-def override_threshold(new):
-    threshold = new
-
 class reg_model(object):
     def __init__(self, X, y, log_trans_y=False, day_cutoff = 70):
         self.X = X
@@ -312,6 +309,41 @@ def series_to_supervised(data, columns, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 
+def generate_prediction_df(level, test_df, test_targs, columns, predictions=20):
+    '''
+    This function generates a prediction array based on the level of social distancing. The predetermined sets
+    are determined as follows: High: Minimum values for each column (except residential),
+    # Medium: inbetween values, Low: 1 (Back to Normal)
+    '''
+    levelDict = {'High': [0.34, 0.5, 0.36, 0.295, 0.4, 1.3, 0.385],
+                 'Medium': [0.6, 0.8, 0.7, 0.7, 0.75, 1.1, 0.7],
+                 'Low': [1, 1, 1, 1, 1, 0.9, 1]
+                 }
+
+    if level not in levelDict.keys():
+        pred_params = level
+    else:
+        pred_params = levelDict[level]
+
+    pred_df = rf_model.X.loc[:, 'days_elapsed(t)':].copy()
+    pred_df['moving_average'] = rf_model.y
+    pred_df.columns = columns
+    last_recorded_day = int(test_df['days_elapsed(t)'].max())
+    for i in range(last_recorded_day + 1, last_recorded_day + predictions + 1):
+        pred_df_row = pd.DataFrame([i] + pred_params).T
+        pred_df_row.columns = columns[:-1]
+        pred_df = pred_df.append(pred_df_row, sort=True)
+    pred_df = pred_df[columns]
+    prediction_ts = series_to_supervised(
+        pred_df, columns=columns, n_in=0, n_out=21, dropnan=False)
+    prediction_ts = prediction_ts[prediction_ts['days_elapsed(t)'] <= 82]
+
+    #Filling in Nan in former time series fields for moving average with the average values seen - to fix later
+    avg_mov_average = prediction_ts.iloc[:6, -1].mean()
+    prediction_ts.fillna(avg_mov_average, inplace=True)
+    prediction_ts.drop('moving_average(t+20)', axis=1, inplace=True)
+    return prediction_ts
+
 if __name__ == '__main__':
     covid_df = load_and_clean_data()
     mask1 = (covid_df['state'] == 'New York')
@@ -330,6 +362,7 @@ if __name__ == '__main__':
     revised_df = replace_with_moving_averages(
         revised_df, revised_df.columns[1:-1])
 
+    revised_df.drop('pop_density', axis= 1, inplace = True)  #Only one state is currently considered in this study
     values = revised_df.values
     ts_frame_data = series_to_supervised(values, revised_df.columns, 20, 1)
     ts_y = ts_frame_data.pop('moving_average(t)')
@@ -338,4 +371,4 @@ if __name__ == '__main__':
     rf_model.rand_forest(n_trees= 'optimize')
     rf_model.evaluate_model()
 
-
+    #Plots in notebooks/EDA.ipynb
